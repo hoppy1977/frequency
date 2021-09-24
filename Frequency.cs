46f885c1faae89e39794d8d12c76b353a15aa5e4
Shane Hopcroft <shanehopcroft@hotmail.com>
2021-09-24 21:53:18 +1000

    Initial upload

---
+++ Frequency.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace frequency
+{
+    class Frequency
+    {
+        private readonly Random _random = new Random();
+
+        public int[] GenerateArray()
+        {
+            var _integerArray = Enumerable
+                .Repeat(0, 100)
+                .Select(i => _random.Next(1, 10))
+                .ToArray();
+
+            return _integerArray;
+        }
+
+        public Dictionary<int, int> FindFrequencies(int[] integerArray)
+        {
+            var frequencies = new Dictionary<int, int>();
+
+            foreach (int x in integerArray)
+            {
+                if (frequencies.ContainsKey(x))
+                {
+                    frequencies[x]++;
+                }
+                else
+                {
+                    frequencies.Add(x, 1);
+                }
+            }
+
+            return frequencies;
+        }
+
+        public void PrintResults(Dictionary<int, int> frequencies)
+        {
+            foreach (var frequency in frequencies.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"Number {frequency.Key} occured {frequency.Value} times");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _frequency = new Frequency();
+
+            var integerArray = _frequency.GenerateArray();
+            var frequencies = _frequency.FindFrequencies(integerArray);
+            _frequency.PrintResults(frequencies);
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
